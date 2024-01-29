ea0515e8a21c2512a8136a27531806e66b87f51a
Steve <steve.raharison@gmail.com>
2024-01-29 13:47:05 +0300

    Main

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from controller import Controller
 
+controller = Controller()
+
+first_action = controller.view.get_menu_choice()
+
+if first_action == 1:
+    player_infos = controller.view.get_player_infos()
+    player = controller.create_player(*player_infos)
+    print(f"Joueur créé : {player}")

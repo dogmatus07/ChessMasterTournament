4ff513bf9774f7f9f1eb29252d713aee74c389ca
Steve <steve.raharison@gmail.com>
2024-02-03 10:32:48 +0300

    Main

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
 from controller import Controller
+import time
 
 controller = Controller()
 
-first_action = controller.view.get_menu_choice()
+"""
+Menu
+"""
+while True:  # boucle pour le menu principal
+    controller.clear_screen()
+    main_menu_choice = controller.view.display_main_menu()
+    if main_menu_choice == 1:
+        controller.clear_screen()
+        while True:  # boucle pour le sous-menu tournoi (1)
+            tournament_menu_choice = controller.view.display_menu_tournament()
+            if tournament_menu_choice == 1:
+                controller.clear_screen()
+                tournament_infos = controller.view.get_tournament_infos()
+                controller.view.display_progress_bar()
+                controller.create_tournament(*tournament_infos)
+                time.sleep(2)
+                controller.clear_screen()
 
-if first_action == 1:
-    player_infos = controller.view.get_player_infos()
-    player = controller.create_player(*player_infos)
-    print(f"Joueur créé : {player}")
+            elif tournament_menu_choice == 2:
+                controller.clear_screen()
+                controller.resume_tournament()
+
+            elif tournament_menu_choice == 3:
+                controller.clear_screen()
+                controller.view.display_main_menu() # back to the main menu
+            else:
+                print("Choix non valide")
+    if main_menu_choice == 2:
+        while True: # boucle pour le sous-menu joueurs
+            controller.clear_screen()
+            player_menu_choice = controller.view.display_menu_players()
+            if player_menu_choice == 1:
+                pass
+
+            elif player_menu_choice ==2:
+                pass
+    if main_menu_choice == 3:
+        while True: # boucle pour le sous-menu rapports
+            player_menu_choice = controller.view.display_menu_players()
+            if player_menu_choice == 1:
+                pass
+
+            elif player_menu_choice ==2:
+                pass
+    if main_menu_choice == 4:
+        break

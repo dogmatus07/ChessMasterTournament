3d17ecd8cdd0e2ad276b555428b7b6210f8c3334
Steve <steve.raharison@gmail.com>
2024-03-12 17:40:41 +0300

    Models

--- models.py
+++ models.py
@@ -45,7 +45,6 @@ class Tournament:
                 f"End Date : {self.end_date.strftime('%d/%m/%Y')}\n"
                 f"Description : {self.description}\n"
                 f"Rounds : {self.round_ids}\n"
-                f"Matches : {self.match_ids}\n"
                 f"Players : {self.player_ids}\n")
 
     def serialize(self):
@@ -56,7 +55,6 @@ class Tournament:
             "end_date": self.end_date.strftime("%d/%m/%Y"),
             "description": self.description,
             "rounds": self.round_ids,
-            "matches": self.match_ids,
             "players": self.player_ids,
             "status": self.status.name
         }
@@ -71,7 +69,6 @@ class Tournament:
                    end_date,
                    data["description"],
                    data.get('rounds', []),
-                   data.get('matches', []),
                    data.get('players', []),
                    )
 
@@ -94,21 +91,19 @@ class Round:
                  tournament_id,
                  match_ids=None,
                  player_ids=None,
-                 matches=[],
+                 matches=None,
+                 start_date="To be defined",
+                 end_date="To be defined",
                  status=RoundStatus.PENDING,
                  doc_id=None):
         self.doc_id = doc_id
         self.tournament_id = tournament_id
-        self.matches = []  # list of matches of current round
+        self.matches = matches if matches is not None else []  # list of matches of current round
         self.match_ids = []  # will contain match ids for the round
+        self.start_date = start_date
+        self.end_date = end_date
         self.player_ids = []  # will contain player_ids who play for that round
         self.status = RoundStatus.PENDING  # initial state of the round
-
-    def set_start_round_status(self):
-        self.status = RoundStatus.IN_PROGRESS
-
-    def set_end_round_status(self):
-        self.status = RoundStatus.FINISHED
 
     def __str__(self):
         return (f"round_id : {self.doc_id}\n"
@@ -116,6 +111,8 @@ class Round:
                 f"match_ids : {self.match_ids}\n"
                 f"player_ids : {self.player_ids}\n"
                 f"matches : {self.matches}\n"
+                f"start_date : {self.start_date}\n"
+                f"end_date : {self.end_date}\n"
                 f"status : {self.status.name}\n")
 
     def serialize(self):
@@ -124,6 +121,8 @@ class Round:
             "match_ids": self.match_ids,
             "player_ids": self.player_ids,
             "matches": self.matches,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
             "status": self.status.name
         }
 
@@ -134,6 +133,8 @@ class Round:
                    match_ids=data.get('match_ids', []),
                    player_ids=data.get('player_ids', []),
                    matches=data.get('matches', []),
+                   start_date=data.get('start_date, []'),
+                   end_date=data.get('end_date', []),
                    status=status
                    )
 
@@ -181,14 +182,6 @@ class Match:
                    data['player2_id'],
                    data['winner'],
                    match_id=match_id)
-
-    def set_match_result(self, winner_id):
-        """
-        Winner : 1
-        Loser : 0
-        Draw : 0.5 for each player
-        """
-        self.winner = winner_id  # id of the winner
 
 
 class Player:
